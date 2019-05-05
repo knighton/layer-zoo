0d348abf06a985e002a12a89d3af2adc9a42ae15
James Knighton <iamknighton@gmail.com>
2019-05-04 17:27:14 -0700

    Noop.

--- pylaser/layer/arch/__init__.py
+++ pylaser/layer/arch/__init__.py
@@ -1,4 +1,5 @@
 from .append import *
 from .fork import *
+from .noop import *
 from .sequence import *
 from .skip import *
---
+++ pylaser/layer/arch/noop.py
@@ -0,0 +1,5 @@
+from ..base import Layer
+
+
+class Noop(Layer):
+    pass
